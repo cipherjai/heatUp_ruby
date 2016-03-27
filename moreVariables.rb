7f13031e589ff9577292aaea47982c84db1935b8
cipherjai <jaiatmart@gmail.com>
2016-03-27 12:18:01 +0530

    changed Variables

--- moreVariables.rb
+++ moreVariables.rb
@@ -1,16 +1,17 @@
-myName = 'Surya Gupta'
-myAge = 35 # not a lie in 2009
-myHeight = 74 #inches
-myWeight = 180 # lbs
-myEyes = 'blue'
-myTeeth = 'White'
-myHair = 'Brown'
+name = 'Surya Gupta'
+age = 35 # not a lie in 2009
+height = 74 #inches
+weight = 180 # lbs
+eyes = 'blue'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{myName}."
-puts "He's #{myHeight} inches tall."
-puts "He's #{myWeight} pounds heavy"
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy"
 puts "Actually that's not too heavy."
-puts "He'sgot #{myEyes} eyes and #{myHair} hair."
-puts "His teeth are usually #{myTeeth} depending on the coffee."
+puts "He'sgot #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
-puts "If I add #{myAge}, #{myHeight}, and #{myWeight} I get #{myAge + myHeight + myWeight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."

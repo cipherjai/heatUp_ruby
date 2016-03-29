320e8c315f475d4013f48adf6d850f779dbd4f09
cipherjai <jaiatmart@gmail.com>
2016-03-29 22:36:32 +0530

    ex10

---
+++ escapeSequence.rb
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's someone out there who i love the most ...
+<3
+You need some more lines on it ?
+Well,
+I dont have any!
+"""

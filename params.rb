8cd5f64992eb07535dbf480ebeacfc165184c951
cipherjai <jaiatmart@gmail.com>
2016-03-31 21:50:04 +0530

    addedEx1 4

---
+++ params.rb
@@ -0,0 +1,12 @@
+first, second, third, arbitary= ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+puts "Your arbitary value says: #{arbitary}"
+
+print "can you define me now ?"
+str = gets.chomp
+puts "#{str} #{second}"
+
